c210e6a464295df39fe20f2e5fe1e26af88935b2
JonisJohnny <jonatan.ivarsson@icloud.com>
2020-03-26 18:30:16 +0100

    Foundation setup

---
+++ rta/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="App-link" href="https://reactjs.org" rel="noopener noreferrer">Edit</a>
+        <a className="App-link" href="https://reactjs.org" rel="noopener noreferrer">Favorite</a>
+      </header>
+      <body className="App-body">
+        
+      </body>
+    </div>
+  );
+}
+
+export default App;
